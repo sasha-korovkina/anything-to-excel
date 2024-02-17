6fcbfe41c3e4e5e39b722bda31d82974dc0efa4b
Sanya Korovkina <sasha@cmi2i.com>
2024-02-17 14:45:42 +0000

    streamlit interface created

---
+++ PDF-OCR-section-splitter.py
@@ -0,0 +1,2 @@
+
+pdf_file = "C:/Users/sasha/Desktop/PDF Test.pdf"
--- PDF-processor.py
+++ PDF-processor.py
@@ -2,6 +2,8 @@ import streamlit as st
 from PyPDF2 import PdfReader
 from pdf2image import convert_from_bytes
 import io
+import json
+import PyPDF2
 
 def extract_text_from_pdf(uploaded_file):
     text = ""
@@ -15,14 +17,31 @@ def extract_text_from_pdf(uploaded_file):
         st.error(f"An error occurred while extracting text: {str(e)}")
     return text
 
+
+def convert_pdf_to_json(pdf_file):
+    json_structure = []
+
+    with pdf_file:
+        reader = PyPDF2.PdfReader(pdf_file)
+        for page_num in range(len(reader.pages)):
+            page_text = reader.pages[page_num].extract_text()
+
+            # Split the text into paragraphs
+            paragraphs = page_text.split('\n\n')
+
+            # Create a JSON object for each paragraph
+            for paragraph in paragraphs:
+                json_structure.append({"text": paragraph.strip(), "page": page_num + 1})
+
+    return json_structure
 def main():
-    st.title("Simple Streamlit App")
+    st.title("CMI2I PDF Reader")
 
     # Add a file uploader widget
     uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
 
-    # Add a button widget
     button_clicked = st.button("Submit")
+    json_btn = st.button("Convert Text to JSON")
 
     # Check if the button is clicked
     if button_clicked:
@@ -30,10 +49,13 @@ def main():
             # Display the extracted text
             st.subheader("Text Extracted from PDF")
             extracted_text = extract_text_from_pdf(uploaded_file)
-            print(extracted_text)
             st.text(extracted_text)
-        else:
-            st.warning("Please upload a PDF file")
+
+    if json_btn:
+        json_structure = convert_pdf_to_json(uploaded_file)
+        st.subheader("JSON Structure of Text")
+        st.json(json_structure)
+
 
 if __name__ == "__main__":
     main()
