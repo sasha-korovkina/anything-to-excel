1c47066b82ae772253b5fc7f62823518b7842d0e
Sanya Korovkina <sasha@cmi2i.com>
2024-02-17 20:04:36 +0000

    PDF Display

--- PDF-processor.py
+++ PDF-processor.py
@@ -1,4 +1,3 @@
-import streamlit as st
 from PyPDF2 import PdfReader
 from pdf2image import convert_from_bytes
 import io
@@ -7,6 +6,16 @@ import PyPDF2
 import pdfminer
 from pdfminer.high_level import extract_text
 import xml.etree.ElementTree as ET
+import streamlit as st
+from PIL import Image
+import fitz  # PyMuPDF
+from streamlit_drawable_canvas import st_canvas
+import streamlit as st
+import fitz
+import tempfile
+from PIL import Image
+
+#from streamlit_aggrid import AgGrid
 
 def extract_text_from_pdf(uploaded_file):
     text = ""
@@ -31,6 +40,13 @@ def convert_pdf_to_json(pdf_file):
                 json_structure.append({"text": paragraph.strip(), "page": page_num + 1})
     return json_structure
 
+def extract_text_from_selection(pdf_bytes, selection_bbox):
+    doc = fitz.open(stream=pdf_bytes, filetype="pdf")
+    page = doc.load_page(0)  # Assuming first page, change index if needed
+    selected_area = fitz.Rect(selection_bbox)
+    selected_text = page.get_text("text", clip=selected_area)
+    return selected_text
+
 def convert_pdf_to_xml(pdf_file):
     root = ET.Element("document")
     text = extract_text(pdf_file)
@@ -41,6 +57,7 @@ def convert_pdf_to_xml(pdf_file):
     xml_str = ET.tostring(root, encoding="unicode", method="xml")
     return xml_str
 
+
 def main():
     st.title("CMI2I PDF Reader")
 
@@ -50,6 +67,25 @@ def main():
     txt_btn = st.button("Convert PDF to Plain Text")
     json_btn = st.button("Convert PDF to JSON")
     xml_btn = st.button("Convert PDF to XML")
+
+    if uploaded_file is not None:
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(uploaded_file.read())
+            temp_file_path = temp_file.name
+        pdf_doc = fitz.open(temp_file_path)
+        # Check if PDF document is not empty
+        if pdf_doc.page_count > 0:
+            # Select page
+            page_number = st.number_input("Enter page number", value=1, min_value=1, max_value=len(pdf_doc), step=1)
+
+            page = pdf_doc.load_page(page_number - 1)
+            pixmap = page.get_pixmap()
+            img = Image.frombytes("RGB", [pixmap.width, pixmap.height], pixmap.samples)
+            img_bytes = io.BytesIO()
+            img.save(img_bytes, format="PNG")
+            st.image(img_bytes)
+        else:
+            st.write("No pages found in the PDF document.")
 
     # Check if the button is clicked
     if txt_btn:
@@ -68,6 +104,7 @@ def main():
         xml_content = convert_pdf_to_xml(uploaded_file)
         st.subheader("PDF XML Structure")
         st.text_area("XML Content", xml_content, height=400)
+
 
 if __name__ == "__main__":
     main()
