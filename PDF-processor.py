39f3845e54f264f71dd5eedfbcbc7fd037f514e3
Sanya Korovkina <sasha@cmi2i.com>
2024-02-24 20:00:38 +0000

    excel inbuilt macro gen. 3

--- PDF-processor.py
+++ PDF-processor.py
@@ -3,8 +3,36 @@ import win32com.client
 import os
 import pythoncom  # Import pythoncom library
 import xlwings as xw
+import base64
 
-def inject_macro(excel_file_path, macro_code, macro_name):
+def inject_macro(excel_file_path, macro_name):
+    macro_code = '''
+                Sub getDataPDF(FileName As String)
+                ActiveWorkbook.Queries.Add Name:="PDF Query", Formula:= _
+                    "let" & Chr(13) & "" & Chr(10) & "    Source = Pdf.Tables(File.Contents(""" & FileName & """), [Implementation=""1.3""])," & Chr(13) & "" & Chr(10) & "    #""Expanded Data"" = Table.ExpandTableColumn(Source, ""Data"", {""Column1"", ""Column2"", ""Column3"", ""Column4"", ""Column5""}, {""Data.Column1"", ""Data.Column2"", ""Data.Column3"", ""Data.Column4"", ""Data.Column5""})" & Chr(13) & "" & Chr(10) & "in" & Chr(13) & "" & Chr(10) & "    #""Expanded Data"""
+                ActiveWorkbook.Worksheets.Add
+                With ActiveSheet.ListObjects.Add(SourceType:=0, Source:= _
+                    "OLEDB;Provider=Microsoft.Mashup.OleDb.1;Data Source=$Workbook$;Location=""Task pdf"";Extended Properties=""""" _
+                    , Destination:=Range("$A$1")).QueryTable
+                    .CommandType = xlCmdSql
+                    .CommandText = Array("SELECT * FROM [PDF Query]")
+                    .RowNumbers = False
+                    .FillAdjacentFormulas = False
+                    .PreserveFormatting = True
+                    .RefreshOnFileOpen = False
+                    .BackgroundQuery = True
+                    .RefreshStyle = xlInsertDeleteCells
+                    .SavePassword = False
+                    .SaveData = True
+                    .AdjustColumnWidth = True
+                    .RefreshPeriod = 0
+                    .PreserveColumnInfo = True
+                    '.ListObject.DisplayName = "Task_pdf"
+                    .Refresh BackgroundQuery:=False
+                End With
+            End Sub
+        '''
+
     # Initialize the COM library for the current thread
     pythoncom.CoInitialize()
 
@@ -25,10 +53,10 @@ def inject_macro(excel_file_path, macro_code, macro_name):
 def run_excel_macro_with_parameter(file_path, macro_name, pdf_file):
     wb = xw.Book(file_path)
     app = xw.App(visible=False)
+    macro = wb.macro(macro_name)
 
     try:
-        # Execute the macro with the parameter
-        wb.macro(macro_name)(pdf_file)
+        macro(pdf_file)
     except Exception as e:
         error_message = str(e)
         raise
@@ -38,69 +66,28 @@ def run_excel_macro_with_parameter(file_path, macro_name, pdf_file):
 def main():
     st.title('Excel Macro Generator')
 
-    # If at any point we want the user to generate their own macros
-    # macro_code = st.text_area("Input your macro code here:", '''
-    # Sub SampleMacro()
-    #     MsgBox "This is a sample macro!"
-    # End Sub
-    # ''', height=300)
+    uploaded_files = st.file_uploader("Upload PDF Files", accept_multiple_files=True)
 
-    macro_code = '''
-            Sub getDataPDF(FileName As String)
-            ActiveWorkbook.Queries.Add Name:="PDF Query", Formula:= _
-                "let" & Chr(13) & "" & Chr(10) & "    Source = Pdf.Tables(File.Contents(""" & FileName & """), [Implementation=""1.3""])," & Chr(13) & "" & Chr(10) & "    #""Expanded Data"" = Table.ExpandTableColumn(Source, ""Data"", {""Column1"", ""Column2"", ""Column3"", ""Column4"", ""Column5""}, {""Data.Column1"", ""Data.Column2"", ""Data.Column3"", ""Data.Column4"", ""Data.Column5""})" & Chr(13) & "" & Chr(10) & "in" & Chr(13) & "" & Chr(10) & "    #""Expanded Data"""
-            ActiveWorkbook.Worksheets.Add
-            With ActiveSheet.ListObjects.Add(SourceType:=0, Source:= _
-                "OLEDB;Provider=Microsoft.Mashup.OleDb.1;Data Source=$Workbook$;Location=""Task pdf"";Extended Properties=""""" _
-                , Destination:=Range("$A$1")).QueryTable
-                .CommandType = xlCmdSql
-                .CommandText = Array("SELECT * FROM [PDF Query]")
-                .RowNumbers = False
-                .FillAdjacentFormulas = False
-                .PreserveFormatting = True
-                .RefreshOnFileOpen = False
-                .BackgroundQuery = True
-                .RefreshStyle = xlInsertDeleteCells
-                .SavePassword = False
-                .SaveData = True
-                .AdjustColumnWidth = True
-                .RefreshPeriod = 0
-                .PreserveColumnInfo = True
-                '.ListObject.DisplayName = "Task_pdf"
-                .Refresh BackgroundQuery:=False
-            End With
-        End Sub
-    '''
-
-    if st.button('Generate Excel File with Macro'):
-        # Temporary path for the generated Excel file
-        excel_file_path = os.path.join(os.getcwd(), "sasha.xlsm")
-
-        # Generate the Excel file
-        inject_macro(excel_file_path, macro_code, "pdfLoader")
-
-    if st.button('Run Macro without Displaying Excel'):
-        try:
+    if st.button('Generate Excel Files with Macros') and uploaded_files:
+        for pdf_file in uploaded_files:
             # Temporary path for the generated Excel file
-            excel_file_path = os.path.join(os.getcwd(), "sasha.xlsm")
+            excel_file_path = os.path.join(os.getcwd(), f"{pdf_file.name.split('.')[0]}.xlsm")
 
-            # Execute the macro within the Excel
-            macro_name = "pdfLoader"
-            pdf_file = r"M:\CDB\Analyst\Rhys\Data\Goldman Sachs Europe DDMMYYYY 30032023 MDXHEALTH S.A._BE0003844611_21-Mar-2023.pdf_decrypted.pdf"
-            run_excel_macro_with_parameter(excel_file_path, macro_name, pdf_file)
-        finally:
-            st.success("Macro executed successfully!")
-    if st.button('Download'):
-        with open(excel_file_path, "rb") as file:
-            btn = st.download_button(
-                label="Download Excel File with Macro",
-                data=file,
-                file_name="GeneratedExcelFile.xlsm",
-                mime="application/vnd.ms-excel.sheet.macroEnabled.12"
-            )
+            # Generate the Excel file
+            inject_macro(excel_file_path, "pdfLoader")
 
-        if btn:
-            st.success("Download started!")
+            # Display a download button for the Excel file
+            download_button_str = generate_download_button(excel_file_path, pdf_file.name.split('.')[0])
+            st.markdown(download_button_str, unsafe_allow_html=True)
+
+        st.success("Excel files generated successfully!")
+
+def generate_download_button(file_path, file_name):
+    with open(file_path, "rb") as file:
+        bytes_data = file.read()
+    b64_data = base64.b64encode(bytes_data).decode()
+    download_button_str = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64_data}" download="{file_name}.xlsm"><button>Download {file_name}</button></a>'
+    return download_button_str
 
 if __name__ == "__main__":
     main()
